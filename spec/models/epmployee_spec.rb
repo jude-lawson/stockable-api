e3b6242875e57ff46dfd13865a241bde113417ef
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-28 11:28:21 -0600

    Adds in enum for Employee role

--- spec/models/epmployee_spec.rb
+++ spec/models/epmployee_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Employee do
+  describe 'Validations' do
+    it { should define_enum_for(:role) }
+  end
+  
   describe 'Relationships' do
     it { should belong_to(:user) }
     it { should belong_to(:store) }
